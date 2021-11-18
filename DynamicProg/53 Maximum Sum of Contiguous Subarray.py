5a47fc6ff9519e27af10c42bd2d7e59976e01143
Thang Tran <tranhuythang@gmail.com>
2021-11-17 23:41:05 -0700

    added solution for Maximum sum of contiguous subarray

--- DynamicProg/53 Maximum Sum of Contiguous Subarray.py
+++ DynamicProg/53 Maximum Sum of Contiguous Subarray.py
@@ -1,20 +1,20 @@
-def maxSubarray(arr):
+def maxSubnumsay(nums):
     """
-    53 Maximum Sum of Contiguous Subarray
-    Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest
+    53 Maximum Sum of Contiguous Subnumsay
+    Given an integer numsay nums, find the contiguous subnumsay (containing at least one number) which has the largest
     sum and return its sum.
-    A subarray is a contiguous part of an array.
+    A subnumsay is a contiguous part of an numsay.
 
-    :param arr:
+    :param nums:
     :return:
     """
-    p = [0 for i in range(len(arr))]
-    maxSubArraySum = p[0] = arr[0]
-    for n in range(len(arr) - 1):
-        p[n+1] = arr[n+1] + max(0, p[n])
-        maxSubArraySum = max(p[n+1], maxSubArraySum)
-    return maxSubArraySum
+    p = [0 for i in range(len(nums))]
+    maxSubnumsaySum = p[0] = nums[0]
+    for n in range(len(nums) - 1):
+        p[n+1] = nums[n+1] + max(0, p[n])
+        maxSubnumsaySum = max(p[n+1], maxSubnumsaySum)
+    return maxSubnumsaySum
 
 test = [[-2,1,-3,4,-1,2,1,-5,4], [5,4,-1,7,8], [1, 1, -7, 3, -1, 2]]
 for s in test:
-    print(s, '->', maxSubarray(s))
+    print(s, '->', maxSubnumsay(s))
