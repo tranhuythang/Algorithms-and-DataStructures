8d3eed0ee48f70a10cd10e4d412dd24f1abc357f
Thang Tran <tranhuythang@gmail.com>
2021-12-22 23:46:35 -0700

    added Sliding Window solution for 239 Sliding Window Max problem

--- Sliding window/239 Sliding Window Maximum.py
+++ Sliding window/239 Sliding Window Maximum.py
@@ -1,5 +1,5 @@
 """
-*Problem: You are given an array of integers nums, there is a sliding window of size k which is moving from the very
+* Problem: You are given an array of integers nums, there is a sliding window of size k which is moving from the very
 left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves
 right by one position. Return the max of all sliding windows.
 Example: Input: nums = [1,3,-1,-3,5,3,6,7], k = 3 -> Output: [3,3,5,5,6,7]
@@ -11,7 +11,8 @@ Window position                Max
  1  3  -1 [-3  5  3] 6  7       5
  1  3  -1  -3 [5  3  6] 7       6
  1  3  -1  -3  5 [3  6  7]      7
-*Algorithm: Brute force takes O(nk) where n = array size, k = window size. The following algorithm is O(n) algorithm.
+
+* Algorithm: Brute force takes O(nk) where n = array size, k = window size. The following algorithm is O(n) algorithm.
 Using sliding window: calculate max of the first sliding window: max(a[0, ..., k-1]). However the max of the next
 sliding window max(a[1, ..., k]) cannot be calculated in O(1) from max(a[0, ..., k-1]).
 
