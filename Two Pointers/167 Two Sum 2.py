a453affff62fe444aee7cee4f951dd39089ef81b
Thang Tran <tranhuythang@gmail.com>
2021-12-22 23:45:52 -0700

    added Two Pointer algorithm for Two Sum 2

--- Two Pointers/167 Two Sum 2.py
+++ Two Pointers/167 Two Sum 2.py
@@ -2,8 +2,8 @@
 *Problem: Given a non-decreasing sorted array of integers numbers[] and an integer k. Find two indices i, j so that
 numbers[i] + numbers[j] == k; return [i+1, j+1] (assume the test works with 1-based indices so add 1 to your result).
 Assume there is only one solution.
-This problem can be solved by using HashMap with O(n) time, O(n) space and not using the array being sorted.
-Use Two Pointers to solve this problem with O(n) time, O(1) space.
+This problem can be solved by using HashMap with O(n) time, O(n) space and not using the array being sorted, but here
+the requirement is using Two Pointers to solve this problem with O(n) time, O(1) space.
 *Algorithm: Use two pointers i = 0 and j = n-1 and then increasing i, decreaing j until we get the result:
     while i < j:
         if numbers[i] + numbers[j] == target:
